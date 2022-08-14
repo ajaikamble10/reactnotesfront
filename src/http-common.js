3be05d25e0da1be661b98fa723689ffd1d2921a4
ajaikamble10 <ajaysuhaskamble@gmail.com>
2022-08-14 09:15:08 +0530

    final changes commit

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   //baseURL: "http://localhost:8080/api",
-  baseURL: "https://springnotesapp.herokuapp.com/api",
+  baseURL: "https://api-notes-app.herokuapp.com/api",
   headers: {
     "Content-type": "application/json",
   },
